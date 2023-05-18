f15a9fa2b70547164c367fbd50f208d2fd947bc8
Taylor Wong <cytaylorw@gmail.com>
2023-05-19 00:36:47 +0800

    Add SQL files

---
+++ sql/demo/demo_init.sql
@@ -0,0 +1,20 @@
+/*
+*  Initial version of database schema
+*    - Create demo DB
+*    - Create demo schema
+*
+*/
+
+USE master;
+GO
+
+-- Create demo DB
+IF (db_id('demo') is null)
+	CREATE DATABASE demo;
+GO
+USE demo;
+
+-- Create demo schema
+IF (SCHEMA_ID('demo') is null)
+	EXEC ('CREATE SCHEMA demo');
+GO
